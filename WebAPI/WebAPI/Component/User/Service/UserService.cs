4ba4f680111d1bdb188d84c0407af0f92789aac5
Bill Bensing <bill@billbensing.com>
2016-05-29 20:29:46 -0400

    Removed generice service and added a UserService template

---
+++ WebAPI/WebAPI/Component/User/Service/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Component.User.Service
+{
+    public class UserService : IUserService
+    {
+       private readonly IUserRepository _userRepository;
+
+        /// <summary>
+        /// Defalut constructor for dependency injection
+        /// </summary>
+        /// <param name="userRepository">An object which implemenents an IUserRepository</param>
+       public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+    }
+}
